daf9fd8bf7f0284a1e64f2a102478c37e45f02ad
Robson Vilaça <robsonvilaca.lima@gmail.com>
2025-05-03 00:08:11 -0300

    teste: UseCase

---
+++ FoodOrder.Pedidos/FoodOrder.Pedidos.Tests/SqsMessageSenderTests.cs
@@ -0,0 +1,65 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using FoodOrder.Pedidos.Infrastructure.Messaging.Producers;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System.Text.Json;
+
+namespace FoodOrder.Pedidos.Tests;
+
+public class SqsMessageSenderTests
+{
+    [Fact]
+    public async Task EnviarMensagemAsync_DeveEnviarMensagemComSucesso()
+    {
+        // Arrange  
+        var mockSqs = new Mock<IAmazonSQS>();
+        var mockConfig = new Mock<IConfiguration>();
+        var fakeQueueUrl = "https://sqs.us-east-1.amazonaws.com/123456789012/checkout";
+
+        // Simula retorno do Amazon SQS  
+        mockSqs.Setup(sqs => sqs.SendMessageAsync(It.IsAny<SendMessageRequest>(), default))
+               .ReturnsAsync(new SendMessageResponse { MessageId = "12345" });
+
+        var sender = new SqsMessageSender(mockSqs.Object, mockConfig.Object);
+        var mensagem = new { Nome = "Teste", Quantidade = 3 };
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var serializedMessage = System.Text.Json.JsonSerializer.Serialize(mensagem, options);
+
+        // Act  
+        await sender.EnviarMensagemAsync(mensagem, fakeQueueUrl);
+    }
+
+    [Fact]
+    public async Task EnviarMensagemAsync_DeveTentarNovamenteSeFalhar()
+    {
+        // Arrange
+        var mockSqs = new Mock<IAmazonSQS>();
+        var mockConfig = new Mock<IConfiguration>();
+        var fakeQueueUrl = "https://sqs.us-east-1.amazonaws.com/123456789012/checkout";
+
+        int callCount = 0;
+        mockSqs.Setup(sqs => sqs.SendMessageAsync(It.IsAny<SendMessageRequest>(), default))
+               .Returns(() =>
+               {
+                   callCount++;
+                   if (callCount < 3)
+                       throw new Exception("Erro simulado");
+                   return Task.FromResult(new SendMessageResponse { MessageId = "12345" });
+               });
+
+        var sender = new SqsMessageSender(mockSqs.Object, mockConfig.Object);
+        var mensagem = new { Nome = "Retry", Quantidade = 1 };
+
+        // Act
+        await sender.EnviarMensagemAsync(mensagem, fakeQueueUrl);
+
+        // Assert
+        mockSqs.Verify(sqs => sqs.SendMessageAsync(It.IsAny<SendMessageRequest>(), default), Times.Exactly(3));
+    }
+}
---
+++ FoodOrder.Pedidos/FoodOrder.Pedidos.Tests/UpdateStatusPedidoCommandHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using FoodOrder.Pedidos.Application.DTOs.Pedidos;
+using FoodOrder.Pedidos.Application.Feature.Pedidos;
+using FoodOrder.Pedidos.Application.UseCase.Pedidos.Interface;
+using FoodOrder.Pedidos.Domain.Enums;
+using MediatR;
+using Moq;
+
+public class UpdateStatusPedidoCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_DeveAtualizarStatusPedido_QuandoPedidoExistir()
+    {
+        // Arrange
+        var pedidoUseCaseMock = new Mock<IPedidoUseCase>();
+
+        var pedido = new PedidoOutput
+        {
+            NumeroPedido = 123,
+            PagamentoStatus = PagamentoStatusEnum.PagamentoRealizado,
+            PedidoStatus = PedidoStatusEnum.EmPreparacao
+        };
+
+        pedidoUseCaseMock
+            .Setup(x => x.Consultar(It.IsAny<int>()))
+            .ReturnsAsync(pedido);
+
+        var handler = new UpdateStatusPedidoCommandHandler(pedidoUseCaseMock.Object);
+
+        var command = new UpdateStatusPedidoCommand(123, PedidoStatusEnum.Pronto);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.Equal(Unit.Value, result);
+        pedidoUseCaseMock.Verify(x => x.Consultar(command.NumeroPedido), Times.Once);
+        pedidoUseCaseMock.Verify(x => x.AtualizarStatusPedido(It.IsAny<PedidoOutput>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_DeveLancarExcecao_SePedidoNaoForEncontrado()
+    {
+        // Arrange
+        var pedidoUseCaseMock = new Mock<IPedidoUseCase>();
+
+        pedidoUseCaseMock
+            .Setup(x => x.Consultar(It.IsAny<int>()))
+            .ReturnsAsync((PedidoOutput?)null);
+
+        var handler = new UpdateStatusPedidoCommandHandler(pedidoUseCaseMock.Object);
+        var command = new UpdateStatusPedidoCommand(999, PedidoStatusEnum.Recebido);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<KeyNotFoundException>(() => handler.Handle(command, default));
+    }
+}
